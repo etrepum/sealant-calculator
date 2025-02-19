8efd113ab2d0b62f3cbf965630e513ce73b00554
etrepum <bob@redivi.com>
2025-02-18 17:08:38 -0800

    URL

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
 import 'virtual:uno.css';
 
-const values = {
-  rimDiameter: 622,
-  tireDiameter: 33,
-  sealantDepth: 300,
+const defaults = {
+  dr: 622,
+  dt: 33,
+  s: 300,
 };
+const values = { ...defaults };
+
+{
+  const url = new URL(window.location.href);
+  for (const k in values) {
+    const qv = url.searchParams.get(k);
+    const n = Number(qv);
+    if (n && !isNaN(n) && isInputId(k)) {
+      values[k] = n;
+    }
+  }
+}
 function isInputId(k: string): k is keyof typeof values {
   return k in values;
 }
 const inputs = [
-  ...document.querySelectorAll('input[id]'),
-] as HTMLInputElement[];
+  ...document.querySelectorAll('input[id],select[id]'),
+] as (HTMLInputElement | HTMLSelectElement)[];
 function recalc() {
-  console.log('recalc');
-  const tire = values.tireDiameter / 2;
-  const rim = values.rimDiameter / 2;
+  const tire = values.dt / 2;
+  const rim = values.dr / 2;
   const outer = rim + tire;
-  const sealantR = values.sealantDepth * 1e-3;
+  const sealantR = values.s * 1e-3;
   const area =
     1000 * (4 * Math.PI * Math.PI * ((rim + tire) / 1000) * (tire / 1000));
   const sealantVolume = sealantR * area;
@@ -33,18 +44,32 @@ function recalc() {
   }
   for (const input of inputs) {
     if (isCalcId(input.id)) {
-      console.log(input.id);
       input.value = calcs[input.id].toFixed(2);
     }
   }
+  const url = new URL(window.location.href);
+  for (const [k, v] of Object.entries(values)) {
+    if (!isInputId(k)) {
+      continue;
+    }
+    if (v === defaults[k]) {
+      url.searchParams.delete(k);
+    } else {
+      url.searchParams.set(k, String(v));
+    }
+  }
+  history.replaceState(history.state, '', url);
 }
 for (const input of inputs) {
   if (isInputId(input.id)) {
     input.value = String(values[input.id]);
     input.addEventListener('change', () => {
       if (isInputId(input.id)) {
-        values[input.id] = +input.value;
-        recalc();
+        const v = Number(input.value);
+        if (v > 0 && !isNaN(v)) {
+          values[input.id] = +input.value;
+          recalc();
+        }
       }
     });
   }
